130714f6549cf29a42390f84659baa08f67b3067
saiakashedara <saiakash.edara@gmail.com>
2026-03-04 10:16:27 +0530

    after pytest issue

--- logic.py
+++ logic.py
@@ -1,24 +1,22 @@
-from logic import *
-
-def test_celsius_to_fahrenheit():
-    assert celsius_to_fahrenheit(0) == 32
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
 
 
-def test_fahrenheit_to_celsius():
-    assert round(fahrenheit_to_celsius(32), 2) == 0
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5/9
 
 
-def test_km_to_miles():
-    assert round(km_to_miles(1), 5) == 0.62137
+def km_to_miles(km):
+    return km * 0.621371
 
 
-def test_miles_to_km():
-    assert round(miles_to_km(1), 5) == 1.60934
+def miles_to_km(miles):
+    return miles / 0.621371
 
 
-def test_kg_to_pounds():
-    assert round(kg_to_pounds(1), 5) == 2.20462
+def kg_to_pounds(kg):
+    return kg * 2.20462
 
 
-def test_pounds_to_kg():
-    assert round(pounds_to_kg(2.20462), 2) == 1
+def pounds_to_kg(lb):
+    return lb / 2.20462
